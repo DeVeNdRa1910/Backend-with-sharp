cc1ff28ebde78f2c66597a203c2afd71fcda7a29
Devendra <devendrav1910@gmail.com>
2024-09-04 12:43:27 +0530

    add cleanup code and split code into multipople functions

--- 1_modeule/2hrProject/server.js
+++ 1_modeule/2hrProject/server.js
@@ -1,41 +1,46 @@
 const http = require('http');
 const fs = require('fs');
 
+// Function to handle serving the form page
+function handleFormRequest(res, message) {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(`<body><h2>${message}</h2>`);
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+}
+
+// Function to handle form submission and writing the message to a file
+function handleFormSubmission(req, res) {
+    const body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    req.on('end', () => {
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split('=')[1].replace(/\+/g, ' ');
+        fs.writeFileSync('message.txt', message);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    });
+}
+
+// Main request listener function
 function rqListener(req, res) {
     const url = req.url;
     const method = req.method;
 
     if (url === '/') {
         fs.readFile('message.txt', (err, data) => {
-            let message = 'No message yet!';  // Default message
-
-            if (!err && data.length > 0) {
-                message = data.toString();
-            }
-
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Enter Message</title></head>');
-            res.write(`<body><h2>${message}</h2>`);  // Display message here
-            res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-            res.write('</html>');
-            return res.end();
+            const message = !err && data.length > 0 ? data.toString() : 'No message yet!';
+            handleFormRequest(res, message);
         });
     } else if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            let message = parseBody.split('=')[1].replace(/\+/g, ' ');
-
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        });
+        handleFormSubmission(req, res);
     } else {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
