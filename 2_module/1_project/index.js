6571808e9f72d6f3521caac2c6409bcc8a3c5907
Devendra <devendrav1910@gmail.com>
2024-09-06 14:00:07 +0530

    use exporess.urlencoded({extended: true})

--- 2_module/1_project/index.js
+++ 2_module/1_project/index.js
@@ -4,16 +4,38 @@ const app = express();
 
 const port = 4040;
 
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
+
+app.use('/', (req, res, next)=>{
+  console.log("This always run"); 
+  next()
+})
+
+app.use('/add-product', (req, res, next)=>{
+  console.log("In perticular middleware");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>') 
+})
+
+app.use('/product',(req, res)=>{
+  console.log(req.body); 
+  res.redirect('/')
+})
+
+app.use((req, res, next)=>{
+  console.log("this is middleware for every route"); 
+  next()
+})
+
 app.get('/', middleware1, middleware2, (req, res)=>{
   res.send("<h1>Welcome to Express JS</h1>")
 })
 
 function middleware1(req, res, next){
-  console.log("ffirst middleware check");
+  console.log("first middleware check");
   next()
 }
 function middleware2(req, res, next){
-  console.log("ffirst middleware check");
+  console.log("second middleware check");
   next()
 }
 
