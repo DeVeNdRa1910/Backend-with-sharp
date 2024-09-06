5cb8ec3e9fa095665383c7a26e1921c365b01a68
Devendra <devendrav1910@gmail.com>
2024-09-06 16:52:40 +0530

    edit somthing in admin routes

--- 2_module/1_project/routes/admin.routes.js
+++ 2_module/1_project/routes/admin.routes.js
@@ -4,7 +4,7 @@ const route = express.Router()
 
 route.use('/add-product', (req, res, next)=>{
   console.log("In perticular middleware");
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>') 
+  res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>') 
 })
 
 route.post('/product',(req, res)=>{
