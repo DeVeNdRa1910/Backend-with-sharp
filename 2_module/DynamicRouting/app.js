07f8ecd7d4fe6d122b384629383f45e92d233ea5
Devendra <devendrav1910@gmail.com>
2024-09-19 21:02:59 +0530

    integrate sequalize

--- 2_module/DynamicRouting/app.js
+++ 2_module/DynamicRouting/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const errorController = require('./controllers/error');
 
@@ -21,5 +21,30 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 
 app.listen(3000);
--- 2_module/DynamicRouting/util/database.js
+++ 2_module/DynamicRouting/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'ecommerce',
-    password: 'bbec2253'
+const sequelize = new Sequelize('ecommerce', 'root', 'bbec2253', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
